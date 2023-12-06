00bb28535cde43e9e0362cceeb2ed3691598d723
Hunter Kam <hone648@gmail.com>
2023-12-06 19:58:04 +0900

    made new data file

---
+++ src/data/data.js
@@ -0,0 +1,50 @@
+export const shampoos = [
+  'Whal USA',
+  "Burt's Bees",
+  'Arm & Hammer',
+  'Honest Paws',
+  'Lilian Ruff',
+];
+
+export const shampooData = [
+  {
+    category: 'Ingredient Integrity',
+    'Wahl USA': 7,
+    "Burt's Bees": 6,
+    'Arm & Hammer': 9,
+    'Honest Paws': 6,
+    'Lilian Ruff': 10,
+  },
+  {
+    category: 'Customer Satisfaction',
+    'Wahl USA': 9,
+    "Burt's Bees": 6,
+    'Arm & Hammer': 8,
+    'Honest Paws': 6,
+    'Lilian Ruff': 4,
+  },
+  {
+    category: 'Effectiveness',
+    'Wahl USA': 7,
+    "Burt's Bees": 6,
+    'Arm & Hammer': 8,
+    'Honest Paws': 6,
+    'Lilian Ruff': 7,
+  },
+  {
+    category: 'Skin and Coat Health',
+    'Wahl USA': 7,
+    "Burt's Bees": 9,
+    'Arm & Hammer': 8,
+    'Honest Paws': 6,
+    'Lilian Ruff': 9,
+  },
+  {
+    category: 'Ease of Use',
+    'Wahl USA': 7,
+    "Burt's Bees": 6,
+    'Arm & Hammer': 5,
+    'Honest Paws': 10,
+    'Lilian Ruff': 7,
+  },
+];
